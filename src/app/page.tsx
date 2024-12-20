47c2f2598fb3ed97e0c1a02a587d5e033ec6d93e
yoonsunny17 <jyoonsun0217@gmail.com>
2024-12-20 12:29:57 +0900

    feat: 인생테마 목록 조회 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import UltimateThemes from "@/components/UltimateThemes";
 import Friends from "@/components/user/Friends";
 import Profile from "@/components/user/Profile";
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-10 py-4">
       <div className="flex flex-col lg:flex-row w-full gap-8">
         {/* 프로필 */}
         <Profile />
         {/* 탈출 메이트 */}
         <Friends />
       </div>
-      <div>{/* 인생테마 */}</div>
+      <div>
+        {/* 인생테마 */}
+        <UltimateThemes />
+      </div>
     </div>
   );
 };
